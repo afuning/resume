9dd92fe13594a6197a6654abe2b0879a7c7c54d5
huning <304873772@qq.com>
2020-01-27 20:18:16 +0800

    chore: 删除resume配置

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -110,10 +110,10 @@ module.exports = {
       // await publishGhPages();
 
       // 调用 Chrome 渲染出 PDF 文件
-      const chromePath = findChrome();
-      spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'http://49.235.122.8:8080/' // 这里注意改成你的在线简历的网站
-      ]);
+      // const chromePath = findChrome();
+      // spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
+      //   'http://49.235.122.8:8080/' // 这里注意改成你的在线简历的网站
+      // ]);
 
       // 重新发布到 ghpages
       // await publishGhPages();
